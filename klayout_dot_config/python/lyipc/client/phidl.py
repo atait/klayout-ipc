bd6fd5b9a365c71a8674e76f7a8975b56edc0eda
Alex Tait <alexander.tait@nist.gov>
2018-07-18 02:53:20 -0600

    make klayout_quickplot a general function

--- klayout_dot_config/python/lyipc/client/phidl.py
+++ klayout_dot_config/python/lyipc/client/phidl.py
@@ -1,39 +1,16 @@
-''' These are functions specific to phidl '''
+''' These are functions specific to phidl
+
+    By importing this module, the client.general module learns how to write phidl.Device
+'''
 from __future__ import print_function
 import phidl
 
-from .general import load, reload, kill, diff
 import time
 import os
-from functools import wraps
 
-
-def klayout_quickplot(device, file, fresh=False, write_load_delay=0.01):
-    ''' Does the write, wait, and load all in one.
-        The fresh argument determines whether to load (True) or reload (False)
-    '''
-    device.write_gds(file)
-    time.sleep(write_load_delay)
-    if fresh:
-        load(file)
-    else:
-        reload()
-
-
-def generate_display_function(default_device, default_file):
-    ''' A quick way to configure quickplotter into a brief command
-
-        Usage::
-            TOP = Device()
-            kqp = make_display_function(TOP, 'debugging.gds')
-            ...
-            kqp()
-    '''
-    default_file = os.path.realpath(default_file)
-    @wraps(klayout_quickplot)
-    def k_quick(device=default_device, file=default_file, **kwargs):
-        klayout_quickplot(device, file, **kwargs)
-    return k_quick
+import lyipc.client.general as general
+general.write_methods.update(phidl.Device='write_gds')
+from .general import load, reload, kill, diff, klayout_quickplot, generate_display_function
 
 
 def trace_phidladd(device, file, write_load_delay=0.01):
--- klayout_dot_config/python/lyipc/client/pya.py
+++ klayout_dot_config/python/lyipc/client/pya.py
@@ -1,46 +1,16 @@
-''' These are functions specific to pya '''
+''' These are functions specific to pya
+
+    By importing this module, the client.general module learns how to write pya.Cell and pya.Layout
+'''
 from __future__ import print_function
 import pya
 
-from .general import load, reload, kill, diff
 import time
 import os
-from functools import wraps
 
-write_methods = {pya.Cell: 'write', pya.Layout: 'write'}
-
-def klayout_quickplot(writable_obj, file, fresh=False, write_load_delay=0.01):
-    ''' Does the write, wait, and load all in one.
-        The fresh argument determines whether to load (True) or reload (False)
-    '''
-    for base_class in type(writable_obj).mro():
-        if base_class in write_methods.keys():
-            write = write_methods[base_class]
-            break
-    else:
-        raise KeyError('write_methods not delared for this class')
-    getattr(writable_obj, write)(file)
-    time.sleep(write_load_delay)
-    if fresh:
-        load(file)
-    else:
-        reload()
-
-
-def generate_display_function(default_writable_obj, default_file):
-    ''' A quick way to configure quickplotter into a brief command
-
-        Usage::
-            TOP = Layout()
-            kqp = make_display_function(TOP, 'debugging.gds')
-            ...
-            kqp()
-    '''
-    default_file = os.path.realpath(default_file)
-    @wraps(klayout_quickplot)
-    def k_quick(writable_obj=default_writable_obj, file=default_file, **kwargs):
-        klayout_quickplot(writable_obj, file, **kwargs)
-    return k_quick
+import lyipc.client.general as general
+general.write_methods = {pya.Cell: 'write', pya.Layout: 'write'}
+from .general import load, reload, kill, diff, klayout_quickplot, generate_display_function
 
 
 def trace_pyainsert(layout, file, write_load_delay=0.01):
