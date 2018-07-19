8cd8f434f9f0366a87d9fbc64598a31f2b4bda69
Alex Tait <alexander.tait@nist.gov>
2018-07-19 17:38:36 -0600

    fixed typo in pya tracer. changed name of the load function

--- klayout_dot_config/python/lyipc/client/pya.py
+++ klayout_dot_config/python/lyipc/client/pya.py
@@ -23,7 +23,7 @@ def trace_pyainsert(layout, file, write_load_delay=0.01):
     pya.Shapes.old_insert = pya.Shapes.insert
     def new_insert(self, *args, **kwargs):
         retval = pya.Shapes.old_insert(self, *args, **kwargs)
-        klayout_quickplot(device, file, fresh=False)
+        klayout_quickplot(layout, file, fresh=False)
         return retval
     pya.Shapes.insert = new_insert
 
