81174ff3b2b722bcc873671f3b1142c51193808b
Alex Tait <atait@princeton.edu>
2018-08-30 23:10:12 -0600

    bumped version after pypi upload error

--- klayout_dot_config/python/setup.py
+++ klayout_dot_config/python/setup.py
@@ -84,7 +84,7 @@ class PostInstall(install):
 
 
 setup(name='lyipc',
-      version='0.1.5',
+      version='0.1.6',
       description='Inter-process communication for Klayout',
       long_description=readme(),
       author='Alex Tait',
