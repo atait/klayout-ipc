652a4847726aee5f0ef240bca1cb25d11a3f8ccc
Alex Tait <alexander.tait@nist.gov>
2018-10-09 14:40:02 -0600

    removed post install hook. this is not a pure python package, so it MUST come through the salt manager. To enter development mode, use the lygadgets linker

--- klayout_dot_config/python/setup.py
+++ klayout_dot_config/python/setup.py
@@ -2,18 +2,6 @@ from setuptools import setup
 import os
 
 
-try:
-    from lygadgets import postinstall_hook
-except (ImportError, ModuleNotFoundError):
-    print('\033[95mlygadgets not found, so klayout package not linked.')
-    print('Please download it in the klayout Package Manager\033[0m')
-    my_postinstall = dict()
-else:
-    setup_dir = os.path.dirname(os.path.realpath(__file__))
-    lypackage_dir = os.path.dirname(setup_dir)
-    my_postinstall = {'install': postinstall_hook(lypackage_dir)}
-
-
 def readme():
     with open('README.rst') as fx:
         return fx.read()
@@ -27,6 +15,6 @@ setup(name='lyipc',
       author_email='alexander.tait@nist.gov',
       license='MIT',
       packages=['lyipc', 'lyipc/client'],
-      install_requires=[],
+      install_requires=['lygadgets'],
       cmdclass=my_postinstall,
       )
