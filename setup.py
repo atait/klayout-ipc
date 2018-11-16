87644ff39fa23dd306adbc0294ddd7a631ce50b4
Alex Tait <alexander.tait@nist.gov>
2018-11-15 17:04:05 -0700

    added version requirement and bumped my version

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readme():
 
 
 setup(name='lyipc',
-      version='0.2.4',
+      version='0.2.5',
       description='Inter-process communication for Klayout',
       long_description=readme(),
       author='Alex Tait',
       author_email='alexander.tait@nist.gov',
       license='MIT',
       packages=['lyipc', 'lyipc/client'],
-      install_requires=['lygadgets'],
+      install_requires=['lygadgets>=0.1.7'],
       package_data={'': ['*.lym']},
       include_package_data=True,
       cmdclass={},
