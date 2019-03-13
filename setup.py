5b16740afa2edd19a8ed82dbd92a1115cc9676a6
Alex Tait <alexander.tait@nist.gov>
2019-03-12 22:03:22 -0600

    version bump 0.2.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lyipc',
-      version='0.2.7',
+      version='0.2.8',
       description='Inter-process communication for Klayout',
       long_description=readme(),
       author='Alex Tait',
