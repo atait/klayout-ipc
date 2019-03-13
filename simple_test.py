58dcae519bd5f9cfab235d7ab51505b37bed7afb
Alex Tait <alexander.tait@nist.gov>
2019-03-12 22:02:49 -0600

    cut out some tests

--- simple_test.py
+++ simple_test.py
@@ -1,18 +1,19 @@
 import lygadgets
+from lygadgets import any_write, any_read
 lygadgets.patch_environment()
+import os
 
-import lyipc
-from lyipc.client.dependent import _get_write_method
-
-# Test pya writables
-import pya
-layout = pya.Layout()
-assert _get_write_method(layout) == layout.write
-TOP = layout.create_cell('TOP')
-assert _get_write_method(TOP) == TOP.write
+def test_writes():
+    # Test pya writables
+    import pya
+    layout = pya.Layout()
+    any_write(layout, 'tempfile.gds')
+    TOP = layout.create_cell('TOP')
+    any_write(TOP, 'tempfile.gds')
 
 
-# Test phidl writables
-import phidl
-box2 = phidl.geometry.rectangle((20, 20))
-assert _get_write_method(box2) == box2.write_gds
+    # Test phidl writables
+    import phidl
+    box2 = phidl.geometry.rectangle((20, 20))
+    any_write(box2, 'tempfile.gds')
+    os.remove('tempfile.gds')
