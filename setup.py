fe40e85362c9796c3753ca41b742a9a18024825d
Alex Tait <atait@princeton.edu>
2022-02-19 18:33:12 -0500

    Version bump 2.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lyipc',
-      version='0.2.9',
+      version='0.2.10',
       description='Inter-process communication for Klayout',
       long_description=readme(),
       author='Alex Tait',
