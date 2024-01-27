c22dd0ae63fd93d1d65c915ff869073b6e7e413c
Alex Tait <atait@ieee.org>
2024-01-27 18:41:20 -0500

    Client uses `socket` instead of PyQt5. That was some dependency eh

--- lyipc/client/__init__.py
+++ lyipc/client/__init__.py
@@ -12,46 +12,19 @@ import socket
 import os
 from lyipc.client.remotehost import set_target_hostname, get_target_hostname
 
-# Determine which socket class to use
-if not isGSI():
-    # print('Warning: pya will not be available')
-    import sys
-    if sys.version_info[0] == 2:
-        raise Exception('Please use python >= 3.1 with lyipc for the time being')
-    elif sys.version_info[0] == 3:
-        import PyQt5.QtNetwork
-        from PyQt5.QtNetwork import QTcpSocket
-        # localhost = '127.0.0.1'
-else:
-    from lygadgets import pya
-    from pya import QTcpSocket
-    # localhost = 'localhost'
 
-
-def send(message='ping 1234', port=None):
+def send(message='ping 1234', port=None, timeout=3):
     ''' Sends a raw message
-
-        Trick here is that PyQt5 does not do automatic str<->byte encoding, but pya does. Also something weird with the addresses
     '''
     if port is None:
         port = lyipc.PORT
     payload = message + '\r\n'
-    psock = QTcpSocket()
-    if not isGSI():
-        payload = payload.encode()
-    psock.connectToHost(get_target_hostname(incl_user=False), port)
-    if psock.waitForConnected():
-        psock.write(payload)
-        if psock.waitForReadyRead(3000):
-            ret = psock.readLine()
-            if not isGSI():
-                ret = bytes(ret).decode('utf-8')
-            handle_query(ret)
-        else:
-            raise Exception('Not acknowledged')
-    else:
-        print('Connection Fail! (tried {}:{})'.format(get_target_hostname(incl_user=False), port))
-    # psock.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(timeout)
+        sock.connect((get_target_hostname(incl_user=False), port))
+        sock.sendall(payload.encode())
+        data = sock.recv(1024).decode()
+    return handle_query(data)
 
 
 def handle_query(retString):
